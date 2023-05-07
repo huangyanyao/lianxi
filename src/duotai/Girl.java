e7f05a9cbc02db4fde3447b3a82d813f5d339a06
Admini <1004085075@qq.com>
2023-05-07 22:49:10 +0800

    第5次提交，多态的练习,注释

--- src/duotai/Girl.java
+++ src/duotai/Girl.java
@@ -7,9 +7,14 @@ public class Girl {
 //    }
 //    public void play(Dog dog){
 //        dog.shout();
+
+
 //    }
     public void play(Animal an){//Animal an = new Cat();
+        System.out.println("11111111111111111");
         an.shout();
     }
+
+
 
 }
